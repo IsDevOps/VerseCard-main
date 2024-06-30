7b16124d6d1b786b1c01d7a9e36ca4439cd8a94c
IsDevOps <issyoseghale@gmail.com>
2024-06-30 19:50:19 +0000

    deploy

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -7,7 +7,6 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Unirest\\' => array($vendorDir . '/mashape/unirest-php/src'),
-    'Mockery' => array($vendorDir . '/mockery/mockery/library'),
     'HTMLPurifier' => array($vendorDir . '/ezyang/htmlpurifier/library'),
     'Detection' => array($vendorDir . '/mobiledetect/mobiledetectlib/namespaced'),
 );
