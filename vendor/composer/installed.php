6e6f8c1248e9cd9e83c87138c13d59dcc9d0a820
IsDevOps <issyoseghale@gmail.com>
2024-06-30 11:41:38 +0000

    deployment

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,11 +1,11 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => 'c8bab64ab78b62633545a9e0868f233e5553ff5f',
         'name' => 'laravel/laravel',
         'dev' => true,
     ),
@@ -617,12 +617,12 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'c8bab64ab78b62633545a9e0868f233e5553ff5f',
             'dev_requirement' => false,
         ),
         'laravel/sail' => array(
